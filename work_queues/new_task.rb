5f6daaca764e7fed128aff355671b34d821eaac5
Dmitriy Nemykin <ddraudred@gmail.com>
2021-04-02 10:31:26 +0300

    Worker queues

---
+++ work_queues/new_task.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'bunny'
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+queue = channel.queue('task_queue', durable: true)
+
+message = ARGV.empty? ? 'Hello, World!' : ARGV.join(' ')
+queue.publish(message, persistent: true)
+puts " [x] Sent #{message}"
+
+connection.close
